2746b045ff1d09e94f9d0566b2a8676423b185c9
dragiev1 <dragievs1@newpaltz.edu>
2024-11-04 15:14:16 -0500

    made a controller and user.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const userController = require("./controllers/users")
 
 const PORT = 3000
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
-});
+}).get("/about", (req, res) => {
+  res.send("About Us")
+}).use("/users", userController)
 
 app.listen(PORT, () => {
   console.log("Server is running on port at http://localhost:" + PORT)
